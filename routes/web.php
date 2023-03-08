95dce2631bc1fcb306fa3c5a731324ad30068681
Luca-Giudice <2giudice4@gmail.com>
2023-03-08 14:17:28 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.home');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.home');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
